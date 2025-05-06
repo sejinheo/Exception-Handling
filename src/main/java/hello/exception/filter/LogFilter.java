8c0a8ba3660b1e837c6d3c79f4a59c9e97af994b
허세진 <heosejin222@gmail.com>
2025-05-06 13:53:31 +0900

    서블릿 예외 처리 - 필터

---
+++ src/main/java/hello/exception/filter/LogFilter.java
@@ -0,0 +1,34 @@
+package hello.exception.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.UUID;
+import jakarta.servlet.*;
+
+@Slf4j
+public class LogFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+        log.info("logFilter init");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
+
+        String uuid = UUID.randomUUID().toString();
+        try {
+            log.info("REQUEST  [{}][{}][{}]", uuid, request.getDispatcherType(), requestURI);
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            log.info("RESPONSE [{}][{}][{}]", uuid, request.getDispatcherType(), requestURI);
+        }
+    }
+}
